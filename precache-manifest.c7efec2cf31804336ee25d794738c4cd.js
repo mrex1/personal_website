a1b776aea4684ce7f60dc1326d7e319253242a27
rex <rexmo@hku.hk>
2020-08-10 09:38:48 +0800

    Updates

---
+++ precache-manifest.c7efec2cf31804336ee25d794738c4cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a8cb9100e12d0c59f86db270b73ca7d",
+    "url": "/rexmo/index.html"
+  },
+  {
+    "revision": "d8ebdeb7b868ced074f1",
+    "url": "/rexmo/static/css/main.06fc3ed7.chunk.css"
+  },
+  {
+    "revision": "a5db4f1e91cacd2332d0",
+    "url": "/rexmo/static/js/2.8f568fd5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rexmo/static/js/2.8f568fd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8ebdeb7b868ced074f1",
+    "url": "/rexmo/static/js/main.1d1e538a.chunk.js"
+  },
+  {
+    "revision": "54ffa1505ee093251b0d",
+    "url": "/rexmo/static/js/runtime-main.89716e09.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dfb4fb647ee7abe8d4785c6785f75e59.js"
+  "/rexmo/precache-manifest.c7efec2cf31804336ee25d794738c4cd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rexmo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
